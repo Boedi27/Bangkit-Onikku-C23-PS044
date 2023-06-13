6485e8d9732bb5ab3ee51d5b03db7227ccf946ca
kheylahakim <130423447+kheylahakim@users.noreply.github.com>
2023-06-13 21:42:41 +0700

    Update app.py

--- Cloud Computing/app.py
+++ Cloud Computing/app.py
@@ -2,44 +2,20 @@ from flask import Flask, request, jsonify
 from PIL import Image, ImageOps
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import img_to_array, ImageDataGenerator, load_img
-#from googleapiclient.discovery import build
-#from google.oauth2 import service_account
 import tensorflow as tf
 import numpy as np
 import io
 import os
 
-#credentials file path and Google Drive file ID
-#credentials_file = 'path/to/credentials.json' #blm
-#drive_file_id = 'your_google_drive_file_id' #blm
-
-#bikin credentials drive
-#credentials = service_account.Credentials.from_service_account_file(credentials_file, scopes=['https://www.googleapis.com/auth/drive']) #diisi sama path nya
-#drive_service = build('drive', 'v3', credentials = credentials)
-
-#req = drive_service.files().get_media(fileId=drive_file_id)
-#file = drive_service.files().get(fileId=drive_file_id).execute()
-#filename = file['machine learning model converted.tflite']
-
-#model_path = 'machine learning model converted.tflite'
-#interpreter = tf.lite.Interpreter(model_path=model_path)
-#interpreter.allocate_tensors()
-
-model = keras.models.load_model('/Users/kheylaramadhanihakim/Downloads/model.h5')
-
-#input and output details
-#input_details = interpreter.get_input_details()
-#output_details = interpreter.get_output_details()
+model = keras.models.load_model('/mode/path/here/.h5')
 
 #image to array
 def transform_image(img):
     imgs = []
-    img = img.resize((256, 256)) #masih kira2
+    img = img.resize((256, 256))
     img_array = img_to_array(img)
-    img_array = img_array.astype(np.float32) / 255 #masih kira2
-   # img_array = tf.image.resize(img_array, [256, 256]) #masih kira2
+    img_array = img_array.astype(np.float32) / 255
     img_array = np.expand_dims(img_array, axis=0)
-
     
     return img_array
 
@@ -75,7 +51,7 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    # Menjalankan aplikasi Flask di port yang ditentukan oleh variabel lingkungan PORT
+    #run Flask
     port = int(os.environ.get("PORT", 5000))
     app.run()
     
